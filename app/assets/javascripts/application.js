20c8c563f58fde704eeed439ff063b85a305f551
Mmadov <mmadov95@gmail.com>
2016-10-25 12:16:22 +0400

    init

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -21,8 +21,3 @@
 //= require owl.carousel
 
 //= require turbolinks
-function add_fields(link, association, content) {
-var new_id = new Date().getTime();
-var regexp = new RegExp("new_" + association, "g");
-$(link).parent().before(content.replace(regexp, new_id));
-}
