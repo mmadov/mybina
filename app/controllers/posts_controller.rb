8dc629305a86e662cbdc9db6cedc2ae2c59f6ba6
mmadov <mmadov95@gmail.com>
2016-10-30 19:37:03 +0400

    init

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,5 +1,5 @@
 class PostsController < ApplicationController
- before_action :authenticate_user!
+ before_action :authenticate_user!, only: [:edit, :create]
   before_action :set_post, only: [:show, :edit, :update, :destroy]
 
   # GET /posts
